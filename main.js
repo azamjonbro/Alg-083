c6bcc8df5baad9a5321ded45c9b7907394d963b6
azamjonbro <azamjondasturchi11@gmail.com>
2025-05-03 12:00:04 +0500

    1232435465643

--- main.js
+++ main.js
@@ -27,29 +27,86 @@
 
 
 
-const user = {
-  name:"Laylo",
-  age: 20,
-  isMarried: false,
-  height:1.59,
-  weight: 40,
-  hobbies: ["reading", "writing", "swimming"],
-  address: {
-    city: "Tashkent",
-    street: "Chilonzor",
-    quarter:"3A",
-    houseNumber: 10,
-    floorNumber: 2,
-  },
-  greet : function(){
-    console.log("Hello, my name is " + this.name);
+// const user = {
+//   name:"Laylo",
+//   age: 20,
+//   isMarried: false,
+//   height:1.59,
+//   weight: 40,
+//   hobbies: ["reading", "writing", "swimming"],
+//   address: {
+//     city: "Tashkent",
+//     street: "Chilonzor",
+//     quarter:"3A",
+//     houseNumber: 10,
+//     floorNumber: 2,
+//   },
+//   greet : function(){
+//     console.log("Hello, my name is " + this.name);
+//   }
+// }
+
+// const newUser = user
+
+// newUser.name ="Madina"
+
+// console.log(user);
+
+// newUser.greet()
+
+
+// "".toLowerCase()
+// function getCount(str) {
+//   let unlilar = str.split("").filter((e)=>{return e=="a"||e=="e"||e=="i"||e=="u"||e=="o"})
+//   return unlilar.length
+// }
+
+// console.log(getCount("abrakadabra"));
+
+
+// function disemvowel(str) {
+//   let undoshlar =str.split("").filter((e)=>{return (e=="a"||e=="e"||e=="i"||e=="u"||e=="o"||e=="A"||e=="E"||e=="I"||e=="U"||e=="O")? false:true})
+//   return undoshlar.join("")
+// }
+// console.log(disemvowel("This website is for losers LOL!"));
+
+// function filter_list(l) {
+//   return l.filter((e)=>{ return typeof e == "number"})
+// }
+
+// function descendingOrder(n){
+//    let n = n.toString().split("").sort((a,b)=>{return b-a}).join("")
+//    return Number(n)
+// }
+
+// console.log(descendingOrder(123456789));
+
+var isSquare = function (n) {
+  if (n >= 0) {
+    let son = Math.sqrt(n)
+    let qiymat = son.toString().includes(".")
+    if (qiymat) {
+      return false
+    } else {
+      return true
+    }
+  }
+  else {
+    return false
   }
 }
+console.log(isSquare(3));
 
-const newUser = user
-
-newUser.name ="Madina"
-
-console.log(user);
-
-newUser.greet()
+function solution(number, limit) { 
+  let arr =[]
+  for (let i = 1; i<=10; i++){
+    for (let j = 1; j<=10; j++){
+      let son = i*j
+      if (son <= limit){
+        arr.push(son)
+      }
+    }
+  }
+  console.log(arr);
+}
+console.log(solution(3, 5))
